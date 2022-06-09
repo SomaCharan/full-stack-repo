83edcccd5a33ac96a4929faaf6c471cd5a60315a
shubham arora <shubhamarora306@gmail.com>
2022-06-09 23:52:34 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -58,9 +58,18 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).send(err.message || err);
 });
 
-app.get("/", function (req, res, next) {
-  res.send("isDevelopment = " + isDevelopment);
-});
+// app.get("/", function (req, res, next) {
+//   res.send("isDevelopment = " + isDevelopment);
+// });
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //listen
 app.listen(port, () => {
